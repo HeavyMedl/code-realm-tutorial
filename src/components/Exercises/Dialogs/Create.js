dcdfb40366b60e11e3e9a1fff0c3261b11399573
Kurt Medley <kurtdmedley@gmail.com>
2019-05-06 20:48:11 -0700

    #6 - Adding the first form field. Using hooks instead of class component

--- src/components/Exercises/Dialogs/Create.js
+++ src/components/Exercises/Dialogs/Create.js
@@ -6,13 +6,27 @@ import {
   DialogContentText,
   DialogTitle,
   Button,
-  Fab
+  Fab,
+  TextField
 } from "@material-ui/core";
 
 import { Add } from "@material-ui/icons";
 
 export default props => {
   const [open, setOpen] = useState(false);
+  const [dialogForm, setDialogForm] = useState({
+    title: "",
+    description: "",
+    muscles: ""
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setDialogForm({
+      ...dialogForm,
+      [name]: value
+    });
+  };
+
   return (
     <>
       <Fab aria-label="Add" size="small" onClick={() => setOpen(!open)}>
@@ -22,6 +36,16 @@ export default props => {
         <DialogTitle id="form-dialog-title">Create a New Exercise</DialogTitle>
         <DialogContent>
           <DialogContentText>Please fill out the form below.</DialogContentText>
+          <form>
+            <TextField
+              id="standard-name"
+              label="Name"
+              name="title"
+              value={dialogForm.title}
+              onChange={handleChange}
+              margin="normal"
+            />
+          </form>
         </DialogContent>
         <DialogActions>
           <Button color="primary" variant="contained">
